4deec6d75cb9fa1e36c40f6a04d3161ba20126c9
DarkLab <allihov67@gmail.com>
2016-04-21 19:31:57 +0300

    refactor RequestMaker

--- ClothesForecast/app/src/main/java/com/khasang_incubator/clothesforecast/helpers/RequestMaker.java
+++ ClothesForecast/app/src/main/java/com/khasang_incubator/clothesforecast/helpers/RequestMaker.java
@@ -14,30 +14,9 @@ public class RequestMaker {
     public static final int TYPE_WEATHER = 1;
     public static final int TYPE_FORECAST = 2;
 
-    private final static String BASE_URL = "http://api.openweathermap.org/data/2.5";
-    private final static String WEATHER = "/weather";
-    private final static String FORECAST = "/forecast";
-    private final static String MODE = "json";
-
-    public static final String getWeatherFor(String cityName) {
-        return getRequestFor(cityName, WEATHER);
-    }
-
-    public static final String getForecastFor(String cityName) {
-        return getRequestFor(cityName, FORECAST);
-    }
-
-    private static final String getRequestFor(String cityName, String what) {
-        String request = Uri.parse(BASE_URL + what).buildUpon()
-                .appendQueryParameter("q", cityName)
-                .appendQueryParameter("mode", MODE)
-//                .appendQueryParameter("cnt", Integer.toString(3))
-                .appendQueryParameter("lang", Locale.getDefault().toString())
-                .appendQueryParameter("APPID", AppVariables.API_KEY)
-                .build().toString();
-        Logger.d(request);
-        return request;
-    }
+    private final static String BASE_URL = "http://api.openweathermap.org/data/2.5/";
+    private final static String WEATHER = "weather";
+    private final static String FORECAST = "forecast";
 
     public static String getRequestStringFor(int type, String cityName) {
         String requestType = WEATHER;
