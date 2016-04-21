5f7338098abf5d7ae5244c6eaed6e464c84811d9
DarkLab <allihov67@gmail.com>
2016-04-21 18:58:07 +0300

    научили RequestMaker определять тип запроса (Weather/Forecast) для корректоного составления строки запроса

--- ClothesForecast/app/src/main/java/com/khasang_incubator/clothesforecast/helpers/RequestMaker.java
+++ ClothesForecast/app/src/main/java/com/khasang_incubator/clothesforecast/helpers/RequestMaker.java
@@ -10,6 +10,10 @@ import java.util.Locale;
  * Класс, в котором формируются запросы
  */
 public class RequestMaker {
+    public static final int ERROR = -1;
+    public static final int TYPE_WEATHER = 1;
+    public static final int TYPE_FORECAST = 2;
+
     private final static String BASE_URL = "http://api.openweathermap.org/data/2.5";
     private final static String WEATHER = "/weather";
     private final static String FORECAST = "/forecast";
@@ -32,6 +36,27 @@ public class RequestMaker {
                 .appendQueryParameter("APPID", AppVariables.API_KEY)
                 .build().toString();
         Logger.d(request);
+        return request;
+    }
+
+    public static String getRequestStringFor(int type, String cityName) {
+        String requestType = WEATHER;
+        switch (type) {
+            case TYPE_FORECAST:
+                requestType = FORECAST;
+                break;
+            default:
+                break;
+        }
+
+        String request = Uri.parse(BASE_URL + requestType).buildUpon()
+                .appendQueryParameter("q", cityName)
+                .appendQueryParameter("APPID", AppVariables.API_KEY)
+                .appendQueryParameter("lang", Locale.getDefault().getLanguage())
+                .build().toString();
+
+        Logger.d(request);
+
         return request;
     }
 }
